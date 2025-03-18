01585ae337e2c3cce55d420a89c49d513b85d36e
Thibaut Bournonville <tbournon@k2r2p10.42mulhouse.fr>
2025-03-18 14:50:15 +0100

    pooint virgul'

--- backend/src/models/scoreModel.js
+++ backend/src/models/scoreModel.js
@@ -3,7 +3,7 @@ import db from '../db.js';
 export const addScore = (player_id, score) => {
 	const stmt = db.prepare('INSERT INTO scores (player_id, score) VALUES (?, ?)');
 	return stmt.run(player_id, score);
-}
+};
 
 export const getScores = () => {
 	return db.prepare('SELECT * FROM scores ORDER BY score DESC LIMIT 10').all();
