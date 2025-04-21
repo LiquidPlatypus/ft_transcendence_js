275d62754984714f17a602c15b1dfe06368190c5
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2025-04-21 14:47:57 +0200

    remove logs

--- backend/src/db.js
+++ backend/src/db.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const db = new Database('pong.db', { verbose: console.log });
+const db = new Database('pong.db');
 
 export function setupDatabase() {
 	db.pragma('foreign_keys = ON');
