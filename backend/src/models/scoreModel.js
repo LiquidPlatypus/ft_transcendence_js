572484fe0ad5ec2ca3f5fe002aa924030cad0dfd
Thibaut Bournonville <tbournon@k1r2p1.42mulhouse.fr>
2025-04-22 10:26:20 +0200

    enleve fonctions inutiles

--- backend/src/models/scoreModel.js
+++ backend/src/models/scoreModel.js
@@ -1,20 +1,5 @@
 import db from '../db.js';
 
-export const addScore = (player_id, score) => {
-	const stmt = db.prepare('INSERT INTO scores (player_id, score) VALUES (?, ?)');
-	return stmt.run(player_id, score);
-};
-
-export const getScores = () => {
-	return db.prepare('SELECT * FROM scores ORDER BY score DESC LIMIT 10').all();
-};
-
-export const deleteScore = (scoreId) => {
-	const deleteScoreStmt = db.prepare('DELETE FROM scores WHERE id = ?');
-	const result = deleteScoreStmt.run(scoreId);
-	return result.changes > 0; // Renvoie true si le score a été supprimé
-};
-
 export const getMatchHistoryPong = () => {
 	return db.prepare(`
 	SELECT
