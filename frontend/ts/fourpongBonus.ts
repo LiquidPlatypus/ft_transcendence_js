bd0aa37c4498558a94cb1d463880db17e33ef8f9
Thibaut Bournonville <tbournon@k0r4p14.42mulhouse.fr>
2025-06-27 10:10:53 +0200

    Revert "affiche perdant 4v4 bonus"
    This reverts commit 4f8532af70413fb1a6792e2c73c65222daabe236.

--- frontend/ts/fourpongBonus.ts
+++ frontend/ts/fourpongBonus.ts
@@ -1330,13 +1330,13 @@ class Ball extends Entity{
 
 	private getWinnerAlias(winner: string): string {
 		if (winner === "Joueur 1")
-			return localStorage.getItem('player1Alias') || 'Joueur 1';
+			return "Joueur 1";
 		else if (winner === "Joueur 2")
-			return localStorage.getItem('player2Alias') || 'Joueur 2';
+			return "Joueur 2";
 		else if (winner === "Joueur 3")
-			return localStorage.getItem('player3Alias') || 'Joueur 3';
+			return "Joueur 3";
 		else
-			return localStorage.getItem('player4Alias') || 'Joueur 4';
+			return "Joueur 4";
 	}
 
 	public getPosition() {
