bfa5186b4c639d3692deefb872e6fb21dd35736a
QuLusche <quenquenl.jeux@gmail.com>
2025-07-02 09:29:50 +0200

    test

--- frontend/ts/popstate.ts
+++ frontend/ts/popstate.ts
@@ -65,12 +65,17 @@ const routes: Record<string, RouteHandler> = {
 	},
 };
 export function navigate(path: string, options?: { replace: boolean }) {
-	if (options?.replace || path === '/home') {
+	const currentPath = window.location.pathname;
+
+	// Évite d'ajouter deux fois le même path
+	if (currentPath === path) {
+		history.replaceState({ path }, '', path);
+	} else if (options?.replace || path === '/home') {
 		history.replaceState({ path }, '', path);
 	} else {
-		history.pushState({ path }, '', path); // ajoute une entrée
+		history.pushState({ path }, '', path);
 	}
-	// Déclenchez les écouteurs *avant* d'appeler handleRoute, car handleRoute change le contenu
+
 	navigateListeners.forEach(listener => listener());
 	handleRoute(path);
 }
