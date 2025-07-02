52fabd3b1c39a274c023350c72fd5d74a2d8cbe7
QuLusche <quenquenl.jeux@gmail.com>
2025-07-02 10:01:28 +0200

    fix 43234

--- frontend/ts/chifoumi.ts
+++ frontend/ts/chifoumi.ts
@@ -37,7 +37,7 @@ export async function showPFCMatch(matchType: MatchType) {
 	const player2Input = (document.getElementById("playerAlias2") as HTMLInputElement);
 	
 	if (!player1Input || !player2Input) {
-		navigate('/chifoumi/game/'+matchType, {replace: true});
+		navigate('/chifoumi/select/players', {replace: true});
 		return;
 	}
 
